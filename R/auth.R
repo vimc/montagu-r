66b1183bb5ed5ddbcd2e3b12663a451566d0c701
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-05-18 16:38:26 +0100

    Add a further layer of class attributes

--- R/auth.R
+++ R/auth.R
@@ -94,9 +94,12 @@ montagu_set_default_location <- function(location) {
   montagu$default <- location
 }
 
+## this is going to change several times potentially.
 montagu_location <- function(location = NULL) {
   structure(location %||% montagu$default,
-            class = c("montagu_server", "orderly_api_server"))
+            class = c("montagu_server",
+                      "orderly_api_server",
+                      "orderly_remote_location"))
 }
 
 montagu_GET <- function(...) {
