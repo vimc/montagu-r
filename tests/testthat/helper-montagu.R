1fca075da7edf77abb4e0ea6e03020a33807a76d
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-06-18 15:16:50 +0100

    Start a test server

--- tests/testthat/helper-montagu.R
+++ tests/testthat/helper-montagu.R
@@ -25,5 +25,15 @@ montagu_test_server <- function() {
 }
 
 
+orderly_test_server <- function() {
+  name <- "interactive"
+  port <- 8321
+  path <- orderly:::prepare_orderly_example(name)
+  server <- orderly.server::orderly_server_background(path, port)
+  server$start()
+  server
+}
+
+
 EXAMPLE <- "internal-2017-population-TUV-MHL"
 EXAMPLE_ID <- "20170823-113855-5091025f"
