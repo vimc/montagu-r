0cc8ae4840016ae5fd2a7ffd60ec4d57101250c4
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-06-18 15:16:59 +0100

    Start testing report running

---
+++ tests/testthat/test-reports-run.R
@@ -0,0 +1,49 @@
+context("reports: run")
+
+## Separate out the report running a little.  This is going to be
+## essentially an integration test and I'm not really sure if it will
+## go ok ok travis because I'm getting general undiagnosable failures
+## with the runner within orderly (see all the 'skip_on_travis' tests
+## there).
+
+test_that("run: success", {
+  server <- orderly_test_server()
+  on.exit(server$stop())
+  remote <- montagu_server("testing", "localhost", server$port, orderly = TRUE)
+
+  p <- file.path(server$path, "src", "count", "parameters.json")
+  writeLines(jsonlite::toJSON(list(time = 1, poll = 0.1),
+                              auto_unbox = TRUE), p)
+
+  ans <- montagu_reports_run("count", location = remote, poll = 0.01,
+                             progress = FALSE)
+
+  expect_equal(ans$name, "count")
+  expect_match(
+    ans$id, "^([0-9]{8}-[0-9]{6})-([[:xdigit:]]{4})([[:xdigit:]]{4})$")
+  expect_equal(ans$status, "success")
+})
+
+
+test_that("run: error", {
+  server <- orderly_test_server()
+  remote <- montagu_server("testing", "localhost", server$port, orderly = TRUE)
+
+  p <- file.path(server$path, "src", "count", "parameters.json")
+  writeLines(jsonlite::toJSON(list(time = 1, poll = -1),
+                              auto_unbox = TRUE), p)
+
+  ans <- montagu_reports_run("count", location = remote, poll = 0.01,
+                             progress = FALSE, stop_on_error = FALSE)
+
+  expect_equal(ans$status, "error")
+
+  ## Get the reference error message here:
+  ps <- file.path(dirname(p), "script.R")
+  env <- new.env(parent = .GlobalEnv)
+  cmp <- tryCatch(capture.output(sys.source(ps, chdir = TRUE, envir = env)),
+                  error = identity)
+
+  ## And check that it turns up _somewhere_
+  expect_match(ans$output$stderr, cmp$message, fixed = TRUE, all = FALSE)
+})
