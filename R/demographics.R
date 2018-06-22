16d5531afcfd00bb982eb4f16ecebdd5f4136017
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-06-22 12:01:39 +0100

    Move montagu_demographics_list within session cache

--- R/demographics.R
+++ R/demographics.R
@@ -11,11 +11,19 @@ NULL
 ##' @rdname montagu_demographics
 ##' @param touchstone_id Touchstone identifier
 montagu_demographics_list <- function(touchstone_id, location = NULL) {
-  path <- sprintf("/touchstones/%s/demographics/", touchstone_id)
-  res <- montagu_api_GET(location, path)
-  data_frame(
-    id = vcapply(res, "[[", "id"),
-    name = vcapply(res, "[[", "name"),
-    gendered = vlapply(res, "[[", "gender_is_applicable"),
-    source = vcapply(res, "[[", "source"))
+  location <- montagu_location(location)
+  tryCatch(
+    location$cache$get(touchstone_id, "demographics_list"),
+    KeyError = function(e) {
+      path <- sprintf("/touchstones/%s/demographics/", touchstone_id)
+      res <- montagu_api_GET(location, path)
+      d <- data_frame(
+        id = vcapply(res, "[[", "id"),
+        name = vcapply(res, "[[", "name"),
+        gendered = vlapply(res, "[[", "gender_is_applicable"),
+        source = vcapply(res, "[[", "source"))
+      location$cache$set(touchstone_id, d, "demographics_list")
+      d
+    }
+  )
 }
