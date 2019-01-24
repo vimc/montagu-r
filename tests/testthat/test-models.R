8f129c14bde609b5f3cbc9ef1c55c5d7531c7f33
Wes Hinsley <w.hinsley@imperial.ac.uk>
2019-01-24 14:09:21 +0000

    Whitespace and expect_equal

--- tests/testthat/test-models.R
+++ tests/testthat/test-models.R
@@ -4,23 +4,22 @@ test_that("download models list", {
   location <- montagu_test_server()
   dat <- montagu_models_list(location = location)
   expect_is(dat, "data.frame")
-  expect_equal(names(dat), c("id", "name", "citation", "modelling_group"))
+  expect_equal(names(dat), c("id", "description", "citation", "modelling_group"))
 })
 
 test_that("download correct model id", {
   location <- montagu_test_server()
   dat <- montagu_model_by_id(model_id = "YFIC", location = location)
   expect_is(dat, "list")
-  expect_equal(names(dat), 
+  expect_equal(names(dat),
                c("id", "description", "citation", "modelling_group"))
-  expect_true(dat$id == "YFIC")
+  expect_equal(dat$id, "YFIC")
 })
 
 test_that("download incorrect model id", {
   location <- montagu_test_server()
-  expect_error(montagu_model_by_id(model_id = "YFICZZZ", location = location), 
+  expect_error(montagu_model_by_id(model_id = "YFICZZZ", location = location),
     paste0("Error running request:\n\t - unexpected-error: ",
            "An unexpected error occurred. Please contact support at ",
            "montagu-help@imperial.ac.uk and quote this code: (.*)"))
-               
 })
