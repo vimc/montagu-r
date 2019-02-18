c72d29464c8729f89de0af5688d72c7d93442ecf
Wes Hinsley <w.hinsley@imperial.ac.uk>
2019-02-18 12:12:21 +0000

    Couple more doc fixes

--- R/diseases.R
+++ R/diseases.R
@@ -1,5 +1,9 @@
+##' Diseases have an internal id and a name in Montagu, which are generally,
+##' but not always the same. Occasionally, it is necessary to provide the
+##' disease id to Montagu functions.
 ##' @title Retrieve list of all disease ids and names.
 ##' @param location the montagu server to connect to.
+##' @return data frame containing ids and names of diseases
 ##' @export
 montagu_diseases <- function(location = NULL) {
   res <- montagu_api_GET(location, "/diseases/")
@@ -8,13 +12,17 @@ montagu_diseases <- function(location = NULL) {
     name = vcapply(res, "[[", "name"))
 }
 
+##' Diseases have an internal id and a name in Montagu, which are generally,
+##' but not always the same. Occasionally, it is necessary to provide the
+##' disease id to Montagu functions.
 ##' @title Retrieve disease with given id.
 ##' @param disease_id The disease id.
 ##' @inheritParams montagu_diseases
+##' @return a list of an id and a disease name
 ##' @export
 montagu_disease <- function(disease_id, location = NULL) {
   assert_scalar_character(disease_id)
   path <- sprintf("/diseases/%s/", disease_id)
   res <- montagu_api_GET(location, path)
-  res[c("id", "name")]
+  as.list(res[c("id", "name")])
 }
