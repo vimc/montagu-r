a019e0d471fa170ad772e4314e4cac774079493d
Wes Hinsley <w.hinsley@imperial.ac.uk>
2019-02-19 13:50:22 +0000

    Vignette, docs, and one test improvement

--- tests/testthat/test-models.R
+++ tests/testthat/test-models.R
@@ -4,24 +4,21 @@ test_that("download models list", {
   location <- montagu_test_server()
   dat <- montagu_models(location = location)
   expect_is(dat, "data.frame")
-  expect_equal(names(dat), c("id", "description", "citation", "modelling_group"))
+  expect_equal(sort(names(dat)),
+               c("citation", "description", "id", "modelling_group"))
 })
 
 test_that("download correct model id", {
   location <- montagu_test_server()
   dat <- montagu_model(model_id = "YFIC", location = location)
   expect_is(dat, "list")
-  expect_equal(names(dat),
-               c("id", "description", "citation", "modelling_group"))
+  expect_equal(sort(names(dat)),
+               c("citation", "description", "id", "modelling_group"))
   expect_equal(dat$id, "YFIC")
 })
 
 test_that("download incorrect model id", {
   location <- montagu_test_server()
-  tryCatch({
-    montagu_model(model_id = "YFICZZZ", location = location)
-    fail(message = "Expect to fail, but did not")
-  }, error = function(e) {
-    expect_equal(class(e)[[1]], "montagu_api_error")
-  })
+  expect_error(montagu_model(model_id = "YFICZZZ", location = location),
+               "Unknown model_id with id 'YFICZZZ'")
 })
