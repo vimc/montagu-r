3638f12f2e2e13ec9afaa244ef456bb5d85ea68b
Wes Hinsley <w.hinsley@imperial.ac.uk>
2019-02-26 15:03:43 +0000

    Allow user/pw override for tests

--- tests/testthat/helper-montagu.R
+++ tests/testthat/helper-montagu.R
@@ -2,19 +2,23 @@ montagu_test_reset <- function(clear = FALSE) {
   montagu_server_global_clear()
 }
 
-
-montagu_test_server <- function() {
+montagu_test_server <- function(username = NULL, password = NULL) {
   testthat::skip_on_travis()
   if (!("testing" %in% montagu_server_global_list())) {
     host <- Sys.getenv("MONTAGU_TEST_HOST", "support.montagu.dide.ic.ac.uk")
     port <- Sys.getenv("MONTAGU_TEST_PORT", "10443")
-    username <- Sys.getenv("MONTAGU_TEST_USERNAME", "")
-    if (!nzchar(username)) {
-      testthat::skip("MONTAGU_TEST_USERNAME is not set")
+    if (is.null(username)) {
+      username <- Sys.getenv("MONTAGU_TEST_USERNAME", "")
+      if (!nzchar(username)) {
+        testthat::skip("MONTAGU_TEST_USERNAME is not set")
+      }
     }
-    password <- Sys.getenv("MONTAGU_TEST_PASSWORD", "")
-    if (!nzchar(password)) {
-      testthat::skip("MONTAGU_TEST_PASSWORD is not set")
+    
+    if (is.null(password)) {
+      password <- Sys.getenv("MONTAGU_TEST_PASSWORD", "")
+      if (!nzchar(password)) {
+        testthat::skip("MONTAGU_TEST_PASSWORD is not set")
+      }
     }
 
     server <- montagu_server("testing", host, as.integer(port),
