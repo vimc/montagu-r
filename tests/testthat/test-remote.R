51ab042e46748d165c05a693db38c2ae5a6c75ab
james thompson <james.thompson@imperial.ac.uk>
2019-07-22 10:26:20 +0100

    don't bother testing memeber variable type

--- tests/testthat/test-remote.R
+++ tests/testthat/test-remote.R
@@ -4,9 +4,7 @@ test_that("check members", {
   remote <- montagu_orderly_remote("test_server", "host_name")
 
   expect_true("name" %in% names(remote))
-  expect(is.character(remote$name))
 
   expect_true("location" %in% names(remote))
-  expect(is.character(remote$location))
 })
 
